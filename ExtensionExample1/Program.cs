9f5efc3f93288903fff9ccd6e9bad130f86a5d73
furch23 <furch23@gmail.com>
2022-09-10 12:46:18 +0800

    加入專案檔案。

---
+++ ExtensionExample1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExtensionExample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToRocDateString_A(DateTime.Now, '_'));
+
+            Console.WriteLine(DateTime.Now.ToRocDateString_B('-'));
+
+            Console.ReadKey();
+        }
+
+        static string ToRocDateString_A(DateTime date, char separator)
+        {
+            int year = (date.Year - 1911);
+            return year.ToString() + separator + date.Month + separator + date.Day;
+        }
+    }
+
+    public static class DateTimeExt
+    {
+        // 將 DateTime 物件格式化成中華民國年份的日期字串.
+        public static string ToRocDateString_B(this DateTime date, char separator)
+        {
+            int year = (date.Year - 1911);
+            return year.ToString() + separator + date.Month + separator + date.Day;
+        }
+    }
+}
---
+++ ExtensionExample2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExtensionExample2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 8.11;
+
+            byte[] bytearray = a.ToConverInt32().ToString().PadLeft(5, '0').ToEncodingDefaultByte();
+
+            Console.ReadKey();
+        }
+
+        static string ToRocDateString_A(DateTime date, char separator)
+        {
+            int year = (date.Year - 1911);
+            return year.ToString() + separator + date.Month + separator + date.Day;
+        }
+    }
+
+    public static class Covert
+    {
+        public static byte[] ToEncodingDefaultByte(this string str)
+        {
+            return System.Text.Encoding.Default.GetBytes(str);
+        }
+
+        public static int ToConverInt32(this double a)
+        {
+            return Convert.ToInt32(a);
+        }
+
+        public static int ToConverInt32(this decimal a)
+        {
+            return Convert.ToInt32(a);
+        }
+    }
+}
